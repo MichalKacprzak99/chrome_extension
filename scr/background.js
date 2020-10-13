d845dd927cd0b10b2b29cefecb2327784df8d1b7
Michal Kacprzak <michal.kacprzak999@gmail.com>
2020-10-13 22:26:31 +0200

    logic to the background

--- scr/background.js
+++ scr/background.js
@@ -1,14 +1,29 @@
-// chrome.runtime.onInstalled.addListener(() => {
-//     console.log('onInstalled...');
-//     // create alarm after extension is installed / upgraded
-//     chrome.alarms.create('refresh', { periodInMinutes: 1 });
-//   });
-  
-//   chrome.alarms.onAlarm.addListener((alarm) => {
-//     console.log(alarm.name); // refresh
-//     helloWorld();
-//   });
-  
-//   function helloWorld() {
-//     console.log("Hello, world!");
-//   }
+var query = { highlighted: true, currentWindow: true };
+function callback(tabs) {
+    var text = ""
+    for( const tab of tabs){
+        text += tab.url +"\n"
+    }
+    document.getElementById('info').textContent = text;
+    var target = document.getElementById('info');
+
+    var range, select;
+    if (document.createRange) {
+        range = document.createRange();
+        range.selectNode(target)
+        select = window.getSelection();
+        select.removeAllRanges();
+        select.addRange(range);
+        document.execCommand('copy');
+        select.removeAllRanges();
+    } else {
+        range = document.body.createTextRange();
+        range.moveToElementText(target);
+        range.select();
+        document.execCommand('copy');
+    }
+
+  }
+chrome.commands.onCommand.addListener(function(command) {
+    chrome.tabs.query(query, callback);
+  });
